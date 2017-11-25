7e096ba92f5ac1c54892912a0c16a2890eb74f07
张子扬 <741323965@qq.com>
2017-11-26 01:35:18 +0800

    haha control (unfinish)
    1. 手指滑动一下移动太多
    2. 移动很硬...直接就改position过去的...

---
+++ Assets/Scripts/HAHAController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HAHAController : MonoBehaviour
+{
+    private Rigidbody player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 movement = Input.GetTouch(0).deltaPosition;
+            if (movement.x < 0)
+            {
+                if (transform.position.x > -1.0f)
+                    transform.position = new Vector3(transform.position.x - 0.7f, transform.position.y, transform.position.z);
+            }
+            else if (movement.x > 0)
+            {
+                if (transform.position.x < 1.0f)
+                    transform.position = new Vector3(transform.position.x + 0.7f, transform.position.y, transform.position.z);
+            }
+        }
+    }
+}
