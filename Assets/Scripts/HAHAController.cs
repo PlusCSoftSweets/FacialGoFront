7c3225fae78f80008524faa2753e4cec24719874
张子扬 <741323965@qq.com>
2017-11-30 10:53:49 +0800

    暂时上传

--- Assets/Scripts/HAHAController.cs
+++ Assets/Scripts/HAHAController.cs
@@ -8,24 +8,24 @@ public class HAHAController : MonoBehaviour
 
     void Start()
     {
-        player = GetComponent<Rigidbody>();
+        //player = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-        {
-            Vector2 movement = Input.GetTouch(0).deltaPosition;
-            if (movement.x < 0)
-            {
-                if (transform.position.x > -1.0f)
-                    transform.position = new Vector3(transform.position.x - 0.7f, transform.position.y, transform.position.z);
-            }
-            else if (movement.x > 0)
-            {
-                if (transform.position.x < 1.0f)
-                    transform.position = new Vector3(transform.position.x + 0.7f, transform.position.y, transform.position.z);
-            }
-        }
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        //{
+        //    Vector2 movement = Input.GetTouch(0).deltaPosition;
+        //    if (movement.x < 0)
+        //    {
+        //        if (transform.position.x > -1.0f)
+        //            transform.position = new Vector3(transform.position.x - 0.7f, transform.position.y, transform.position.z);
+        //    }
+        //    else if (movement.x > 0)
+        //    {
+        //        if (transform.position.x < 1.0f)
+        //            transform.position = new Vector3(transform.position.x + 0.7f, transform.position.y, transform.position.z);
+        //    }
+        //}
     }
 }
