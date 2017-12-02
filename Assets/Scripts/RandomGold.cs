ac44edcc15d1ead6dd55d47767a4ffc251d08043
zhengjiafeng <1773643139@qq.com>
2017-12-02 12:55:02 +0800

    add magnet

--- Assets/Scripts/RandomGold.cs
+++ Assets/Scripts/RandomGold.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class RandomGold : MonoBehaviour
 {
-    float CreatTime = 1f;
+    float CreatTime = 4f;
     GameObject gold;
+    public GameObject haha;
 
 
     void Update()
@@ -13,10 +14,11 @@ public class RandomGold : MonoBehaviour
         CreatTime -= Time.deltaTime;    //开始倒计时
         if (CreatTime <= 0)    //如果倒计时为0 的时候
         {
-            CreatTime = Random.Range(0.5f, 1f);     //随机3到9秒内
+            CreatTime = Random.Range(2f, 3f);    
             GameObject obj = (GameObject)Resources.Load("Prefabs/Gold");    //加载预制体到内存
             gold = Instantiate<GameObject>(obj);    //实例化金币
-            gold.transform.position = new Vector3(Random.Range(-6.0f, 5.5f), 0f, Random.Range(-160f, 220f));
+            gold.transform.position = new Vector3(Random.Range(-6.0f, 5.5f), 0f, 
+                Random.Range(haha.transform.localPosition.z - 50f, 150f));
         }
     }
 }
