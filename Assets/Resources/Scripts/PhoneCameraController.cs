7b55357c2e6b5b63c5bee8122ab33fb9a0a7843d
DaddyTrap <914670787@qq.com>
2017-12-03 12:24:00 +0800

    加入魔镜游戏内的Scene

---
+++ Assets/Resources/Scripts/PhoneCameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCameraController : MonoBehaviour {
+
+	private bool cameraAvailable;
+	private WebCamTexture frontCamera;
+	private Texture defaultBackground;
+
+	public RawImage background;
+	public AspectRatioFitter fit;
+
+	// Use this for initialization
+	void Start () {
+		cameraAvailable = false;
+		defaultBackground = background.texture;
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		if (devices.Length == 0) {
+			Debug.Log ("No camera detected");
+			return;
+		}
+			
+		for (int i = 0; i < devices.Length; ++i) {
+			if (devices [i].isFrontFacing) {
+				frontCamera = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+				break;
+			}
+		}
+
+		if (frontCamera == null) {
+			Debug.Log ("No front camera detected");
+			return;
+		}
+
+		frontCamera.Play ();
+		background.texture = frontCamera;
+		cameraAvailable = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!cameraAvailable)
+			return;
+
+		float ratio = (float)frontCamera.width / frontCamera.height;
+		fit.aspectRatio = ratio;
+
+		float scaleY = frontCamera.videoVerticallyMirrored ? -1f : 1f;
+		background.rectTransform.localScale = new Vector3 (1f, scaleY, 1f);
+
+		int orient = -frontCamera.videoRotationAngle;
+		background.rectTransform.localEulerAngles = new Vector3 (0, 180f, orient);
+	}
+}
