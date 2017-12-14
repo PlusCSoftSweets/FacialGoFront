43ab25f7b63fc6880c740baead0bf5aabf83a785
DaddyTrap <914670787@qq.com>
2017-12-15 01:09:36 +0800

    添加登录页面

---
+++ Assets/Resources/Scripts/LoginScene/LoginButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginButton : MonoBehaviour {
+
+	public void OnLoginClick() {
+		SceneManager.LoadScene ("SingelModelScene");
+	}
+}
