a4f85f8b9934440a8578cb0c6a7fda531f06fc7d
Jeff <1773643139@qq.com>
2017-12-15 13:16:53 +0800

    Rotate (#8)
    * add rotating after transform
    
    * conflict fixed.
    
    * done

--- Assets/Resources/Scripts/TransButtonClick.cs
+++ Assets/Resources/Scripts/TransButtonClick.cs
@@ -6,12 +6,21 @@ public class TransButtonClick : MonoBehaviour {
 
     private bool isClick = false;
     private float time = 0;
-	
-	// Update is called once per frame
-	void Update () {
+    GameObject rotator;
+    public GameObject m_Player;  //要跟随的物体
+    int rotatingSpeed = 15;
+
+    void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (time < 0.001 && isClick) {
             cancelReverse();
             isClick = false;
+            Destroy(rotator);
         }
         else if (isClick)
             time -= Time.deltaTime;
@@ -19,14 +28,29 @@ public class TransButtonClick : MonoBehaviour {
 	}
 
     public void buttonClick() {
-        HAHAController player = HAHAController.getHaHaInstance();
-        player.isReverse = true;
-        time = 10;
-        isClick = true;
+        if (rotator == null)
+        {
+            HAHAController player = HAHAController.getHaHaInstance();
+            player.isReverse = true;
+            time = 10;
+            isClick = true;
+            GameObject obj = (GameObject)Resources.Load("Prefabs/Rotator");    //加载预制体到内存
+            rotator = Instantiate<GameObject>(obj);    //实例化加速器
+        }
     }
 
     private void cancelReverse() {
         HAHAController player = HAHAController.getHaHaInstance();
         player.isReverse = false;
+    }
+    void LateUpdate()
+    {
+        if (rotator != null)
+        {
+            rotator.transform.position = new Vector3(m_Player.transform.position.x, 
+                m_Player.transform.position.y + 1.8f, m_Player.transform.position.z);
+            //rotator.transform.Rotate(Vector3.back* rotatingSpeed);
+            rotator.transform.Rotate(0* Time.deltaTime, 0 * Time.deltaTime, -90 * Time.deltaTime * rotatingSpeed);
+        }
     }
 }
