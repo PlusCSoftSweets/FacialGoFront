16fdde61faef1eeb689c772813adbf76dbb46ffd
张子扬 <741323965@qq.com>
2018-02-02 17:35:55 +0800

    开场动画+登陆界面前端

--- Assets/Resources/Scripts/LoginScene/LoginButton.cs
+++ Assets/Resources/Scripts/LoginScene/LoginButton.cs
@@ -5,7 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LoginButton : MonoBehaviour {
 
+    public void OnChangeAccountClick() {
+        Debug.Log("Change Account Click");
+    }
+
+    public void OnCheckClick() {
+        Debug.Log("Check Button Click");
+    }
+
 	public void OnLoginClick() {
-		SceneManager.LoadScene ("SingelModelScene");
-	}
+        Debug.Log("Login Button Click");
+        GameObject.Find("WaittingObject").transform.localPosition = new Vector3(0, 0, 0);
+        GameObject.Find("LoginObject").transform.localPosition = new Vector3(0, 0, -1001);
+        StartCoroutine(FadeScene());
+    }
+
+    IEnumerator FadeScene() {
+        float time = GameObject.Find("Fade").GetComponent<FadeScene>().BeginFade(1);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene("SingelModelScene");
+    }
 }
