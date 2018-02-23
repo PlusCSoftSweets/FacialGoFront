5988960a4dcee6c38c6fd7806235047b49d6364f
jeff <jeff@Ubuntu16.04>
2018-02-23 22:45:47 +0800

    dialog

---
+++ Assets/Resources/Scripts/MainScene/DetailButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetailButtonScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnDetailButtonClick(){
+		Debug.Log("测试按钮");
+	}
+}
