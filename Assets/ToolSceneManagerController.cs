7e49145c48baf3ef1416ed0035230779af821a07
zhengjiafeng <1773643139@qq.com>
2018-03-14 00:18:12 +0800

    tool

---
+++ Assets/ToolSceneManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolSceneManagerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick()
+    {
+        Debug.Log("切换测试");
+    }
+}
---
+++ Assets/raw.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick()
+    {
+        Debug.Log("点击切换");
+    }
+}
