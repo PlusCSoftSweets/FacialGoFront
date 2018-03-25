f9d6c3ffe32a420fd832cc43b28d2c060200e6f1
Jeff <1773643139@qq.com>
2018-03-25 19:13:19 +0800

    Toolfinish (#30)
    * part of tool page
    
    * done
    
    * logic of tool page finish.

--- Assets/ToolSceneManagerController.cs
+++ Assets/ToolSceneManagerController.cs
@@ -2,39 +2,59 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System;
 
 public class ToolSceneManagerController : MonoBehaviour {
     public GameObject DetailCanvas;
     public GameObject ContentCanvas;
+    public GameObject FirstLine;
+    public GameObject SecondLine;
+    public GameObject ThirdLine;
 
-	// Use this for initialization
-	void Start () {
-        //找到按钮，并且获取按钮的Button组件  
-        //Button btn = GameObject.Find("ToolItem (1)").GetComponent<Button>();
-        //注册按钮的点击事件  
+    void Start () {
+        FirstLine.SetActive(true);
+        SecondLine.SetActive(false);
+        ThirdLine.SetActive(false);
         foreach (Transform child in  ContentCanvas.transform)
         {
-            Debug.Log("所有该脚本的物体下的子物体名称:" + child.name);
             Button btn = child.GetComponent<Button>();
+            Transform grand = child.Find("Top");
+            Transform grandgrand = grand.Find("Name");
+            string text = grandgrand.GetComponent<Text>().text;
             btn.onClick.AddListener(delegate () {
-                this.onItemClick();
+                this.onItemClick(text);
             });
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     public void onCloseButtonClick()
     {
         DetailCanvas.SetActive(false);
         Debug.Log("关闭按钮");
     }
-    public void onItemClick()
+    public void onItemClick(string text)
     {
         //Destroy(this.gameObject);
         DetailCanvas.SetActive(true);
-        //this.gameObject.SetActive(false);
+        Transform father = DetailCanvas.transform;
+        Transform title = father.Find("Title");
+        title.GetComponent<Text>().text = text;
+    }
+    public void otherTool()
+    {
+        FirstLine.SetActive(true);
+        SecondLine.SetActive(false);
+        ThirdLine.SetActive(false);
+    }
+    public void myTool()
+    {
+        FirstLine.SetActive(false);
+        SecondLine.SetActive(true);
+        ThirdLine.SetActive(false);
+    }
+    public void advancedTool()
+    {
+        FirstLine.SetActive(false);
+        SecondLine.SetActive(false);
+        ThirdLine.SetActive(true);
     }
 }
