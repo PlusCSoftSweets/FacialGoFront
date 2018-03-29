0a3873f014021ee624d74b7826b71cf93c41635c
张子扬 <741323965@qq.com>
2018-03-29 10:28:05 +0800

    for pull

---
+++ Assets/FaceEditSceneController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+using UnityEngine.SceneManagement;
+
+public class FaceEditSceneController : MonoBehaviour {
+
+    public GameObject leftLine;
+    public GameObject rightLine;
+    public GameObject FacePanel;
+    public GameObject WordPanel;
+    public GameObject Face;
+
+
+    // Use this for initialization
+    void Start () {
+        leftLine.SetActive(true);
+        rightLine.SetActive(false);
+        FacePanel.SetActive(true);
+        WordPanel.SetActive(false);
+        FaceItemClick();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onFacePanelClick()
+    {
+        leftLine.SetActive(true);
+        rightLine.SetActive(false);
+        WordPanel.SetActive(false);
+        FacePanel.SetActive(true);
+    }
+    public void onWordPanelClick()
+    {
+        leftLine.SetActive(false);
+        rightLine.SetActive(true);
+        FacePanel.SetActive(false);
+        WordPanel.SetActive(true);
+    }
+    public void backToMainScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void FaceItemClick()
+    {
+        foreach (Transform child in FacePanel.transform)
+        {
+            Button btn = child.GetComponent<Button>();
+            Image image = child.GetComponent<Image>();
+            Sprite sprite = image.sprite;
+            btn.onClick.AddListener(delegate () {
+                this.onItemClick(sprite);
+            });
+        }
+    }
+    public void onItemClick(Sprite sprite)
+    {
+        Image image = Face.GetComponent<Image>();
+        image.sprite = sprite;
+    }
+}
---
+++ Assets/ToolItemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+
+public class ToolItemController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick()
+    {
+        //Destroy(this.);
+    }
+}
