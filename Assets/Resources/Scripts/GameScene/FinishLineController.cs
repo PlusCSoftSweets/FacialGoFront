38e5141260b22ecc25bc5679933490c8bd5c4cd1
张子扬 <741323965@qq.com>
2018-03-30 20:03:02 +0800

    Finish line debug

--- Assets/Resources/Scripts/GameScene/FinishLineController.cs
+++ Assets/Resources/Scripts/GameScene/FinishLineController.cs
@@ -15,20 +15,37 @@ public class FinishLineController : Photon.PunBehaviour {
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "localHAHA") {
             HAHAController.GetHaHaInstance().isFinish = true;
-            if (PhotonNetwork.RaiseEvent(11, "FINISH", true, null)) {
-                GameOverCalled();
-            }
+            PhotonNetwork.RaiseEvent(20, PhotonNetwork.time, true, null);
         }
     }
 
     #region Private Methods
     private void OnEvent(byte eventcode, object content, int senderid) {
-        if (eventcode == 11) {
-            if (((string)content).Equals("FINISH")) {
-                Debug.Log(HAHAController.GetHaHaInstance().gameObject.name + "LOSSER CALLED");
+        if (eventcode == 20) {
+            Debug.Log(HAHAController.GetHaHaInstance().gameObject.name + "LOSSER CALLED");
+            if (HAHAController.GetHaHaInstance().isFinish) {
+                if (PhotonNetwork.time < (double)content) {
+                    PhotonNetwork.RaiseEvent(21, null, true, null);
+                    GameOverCalled();
+                }
+                else {
+                    PhotonNetwork.RaiseEvent(22, null, true, null);
+                    SetPlayerResult();
+                    GameOverCalled();
+                }
+            } 
+            else {
+                PhotonNetwork.RaiseEvent(22, null, true, null);
                 SetPlayerResult();
                 GameOverCalled();
             }
+        }
+        else if (eventcode == 21) {
+            SetPlayerResult();
+            GameOverCalled();
+        }
+        else if (eventcode == 22) {
+            GameOverCalled();
         }
     }
 
