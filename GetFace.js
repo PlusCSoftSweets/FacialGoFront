2f0b05c6d85d0b010d0982767b11186e15d03ec5
zzz0906 <zzz879978@outlook.com>
2018-04-20 16:45:42 +0800

    face

--- GetFace.js
+++ GetFace.js
@@ -1,21 +1,35 @@
-var https = require('https');
-var qs = require('querystring');
-
-const param = qs.stringify({
-    'grant_type': 'client_credentials',
-    'client_id': 'y2EcSosAzxyGH5EZQnjc9S86',
-    'client_secret': 'wdspPGwbdGqPN8of96CB8gM048Ft8TZF'
-});
-
-https.get(
-    {
-        hostname: 'aip.baidubce.com',
-        path: '/oauth/2.0/token?' + param,
-        agent: false
-    },
-    function (err, res) {
-        // 在标准输出中查看运行结果
-        var response;
-        res.pipe(process.stdout);
+var request = require('request');
+var fs = require('fs');
+const sharp = require('sharp');
+var face_image;
+var formData = {
+    // Pass a simple key-value pair
+    api_key:"J2isE6Vb8xUbud3CSSEiFXqNFsuImDC4",
+    // Pass data via Buffers
+    api_secret: "fy1b1Iz7CAA6HB3BpQHMWl1tvG5Smdm9",
+    image_file: fs.createReadStream(__dirname + '/face.jpg'),
+};
+request.post({url:'https://api-cn.faceplusplus.com/facepp/v3/detect',formData : formData}, function (error, response, body) {
+    //console.log(body);
+    if(error){
+        console.log(error)
     }
-);
+    if (body === undefined){
+
+    }else{
+      res = JSON.parse(body);
+    /*var faces_array = res.faces.map(one => {
+      return one.face_rectangle;
+    });*/
+    //console.log(res.faces);
+    //console.log(faces_array);
+      var one_face = res.faces[0].face_rectangle;
+      console.log(one_face);
+      console.log(one_face.width);
+      sharp("face00.png")
+      .extract({ left: one_face.left, top: one_face.top, width: one_face.widht, height: one_face.height })
+      .toFile("face11.png", function(err) {
+      });
+
+    }
+})
